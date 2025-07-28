96f28ccd8a70b63e81b1ded6cdccbb3911cf95bb
Caroline Frey <cfrey9@gmail.com>
2025-07-28 12:00:35 -0600

    remove storyboard

---
+++ Stacks/Stacks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stacks
+//
+//  Created by Caroline Frey on 7/28/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Stacks/StacksTests/StacksTests.swift
@@ -0,0 +1,17 @@
+//
+//  StacksTests.swift
+//  StacksTests
+//
+//  Created by Caroline Frey on 7/28/25.
+//
+
+import Testing
+@testable import Stacks
+
+struct StacksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
