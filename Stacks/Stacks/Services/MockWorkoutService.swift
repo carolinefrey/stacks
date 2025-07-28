c52754a63679198baeab599ccd1640ef071fb12f
Caroline Frey <cfrey9@gmail.com>
2025-07-28 14:29:19 -0600

    create WorkoutService protocol and mock workout service to return fake data for testing

--- Stacks/Stacks/Services/MockWorkoutService.swift
+++ Stacks/Stacks/Services/MockWorkoutService.swift
@@ -4,4 +4,22 @@
 //
 //  Created by Caroline Frey on 7/28/25.
 //
+import Foundation
 
+class MockWorkoutService: WorkoutService {
+    func fetchWorkouts(forWeekOf: Int) {
+        let workout1 = Workout(title: "Morning CrossFit", type: .crossFit, date: .now, duration: 60, calories: 250)
+        let workout2 = Workout(title: "Morning CrossFit", type: .crossFit, date: .now, duration: 60, calories: 350)
+        let workout3 = Workout(title: "Morning Walk", type: .walk, date: .now, duration: 60, calories: 264)
+        let workout4 = Workout(title: "Morning Lift", type: .strength, date: .now, duration: 60, calories: 104)
+        let workout5 = Workout(title: "Morning CrossFit", type: .crossFit, date: .now, duration: 75, calories: 478)
+        let workout6 = Workout(title: "Morning Run", type: .run, date: .now, duration: 90, calories: 605)
+        let workout7 = Workout(title: "Morning Weighted Walk", type: .weightVestWalk, date: .now, duration: 30, calories: 118)
+
+    }
+    
+    func fetchWorkoutDetails(id: UUID) {
+        let workout = Workout(title: "Morning CrossFit", type: .crossFit, date: .now, duration: 60, calories: 250)
+        print("Fake workout details for id \(id): \(workout)")
+    }
+}
--- Stacks/Stacks/Services/WorkoutService.swift
+++ Stacks/Stacks/Services/WorkoutService.swift
@@ -4,4 +4,9 @@
 //
 //  Created by Caroline Frey on 7/28/25.
 //
+import Foundation
 
+protocol WorkoutService {
+    func fetchWorkouts(forWeekOf: Int)
+    func fetchWorkoutDetails(id: UUID)
+}
