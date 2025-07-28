ef83dc1d904f73a53a18aceb15701f84c7a485a9
Caroline Frey <cfrey9@gmail.com>
2025-07-28 14:17:05 -0600

    create Workout and WorkoutType data models

--- Stacks/Stacks/Models/Workout.swift
+++ Stacks/Stacks/Models/Workout.swift
@@ -5,3 +5,15 @@
 //  Created by Caroline Frey on 7/28/25.
 //
 
+import Foundation
+
+struct Workout {
+    let id: UUID = UUID()
+    let title: String
+    let type: WorkoutType
+    let date: Date // format
+    let duration: Double // will probably want to format depending on how Strava sends this through. Ex: 1h 25min
+    let calories: Int
+    
+    //TODO: add workout-specific values like distance and pace
+}
--- Stacks/Stacks/Models/WorkoutType.swift
+++ Stacks/Stacks/Models/WorkoutType.swift
@@ -5,3 +5,32 @@
 //  Created by Caroline Frey on 7/28/25.
 //
 
+enum WorkoutType: String, CaseIterable {
+    case strength
+    case crossFit
+    case run
+    case walk
+    case weightVestWalk
+    case bike
+    case mountainBike
+    case swim
+    case pickleBall
+    case tennis
+}
+
+extension WorkoutType {
+    var displayName: String {
+        switch self {
+        case .strength: return "Strength"
+        case .crossFit: return "CrossFit"
+        case .run: return "Run"
+        case .walk: return "Walk"
+        case .weightVestWalk: return "Weight Vest Walk"
+        case .bike: return "Bike"
+        case .mountainBike: return "Mountain bike"
+        case .swim: return "Swim"
+        case .pickleBall: return "Pickleball"
+        case .tennis: return "Tennis"
+        }
+    }
+}
