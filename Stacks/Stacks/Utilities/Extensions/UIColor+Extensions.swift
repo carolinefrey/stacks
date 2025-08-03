fd426740399e538d4f8585d49fd414a30274386f
Caroline Frey <cfrey9@gmail.com>
2025-08-03 06:15:35 -0400

    Add hex code extension to UIColor

--- Stacks/Stacks/Utilities/Extensions/UIColor+Extensions.swift
+++ Stacks/Stacks/Utilities/Extensions/UIColor+Extensions.swift
@@ -5,3 +5,22 @@
 //  Created by Caroline Frey on 7/28/25.
 //
 
+import UIKit
+
+extension UIColor {
+    convenience init(hex: String) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        if hexSanitized.hasPrefix("#") {
+            hexSanitized.remove(at: hexSanitized.startIndex)
+        }
+
+        var rgb: UInt64 = 0
+        Scanner(string: hexSanitized).scanHexInt64(&rgb)
+
+        let r = CGFloat((rgb & 0xFF0000) >> 16) / 255
+        let g = CGFloat((rgb & 0x00FF00) >> 8) / 255
+        let b = CGFloat(rgb & 0x0000FF) / 255
+
+        self.init(red: r, green: g, blue: b, alpha: 1.0)
+    }
+}
