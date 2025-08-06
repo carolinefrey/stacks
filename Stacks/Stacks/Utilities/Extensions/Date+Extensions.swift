94ca0bd84ea96688586f53b8af9f195388321356
Caroline Frey <cfrey9@gmail.com>
2025-08-06 07:09:00 -0400

    Add date extensions

--- Stacks/Stacks/Utilities/Extensions/Date+Extensions.swift
+++ Stacks/Stacks/Utilities/Extensions/Date+Extensions.swift
@@ -5,3 +5,21 @@
 //  Created by Caroline Frey on 7/28/25.
 //
 
+import Foundation
+
+extension Date {
+    var fiveYearsAgo: Date? {
+        return Calendar.current.date(byAdding: .year, value: -5, to: Date())
+    }
+    
+    var today: Date? {
+        let components = Calendar.current.dateComponents([.year, .month, .day], from: self)
+        return Calendar.current.date(from: components)
+    }
+    
+    func dayOfWeek() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "EEEE" // Full day name (e.g., Monday)
+        return formatter.string(from: self)
+    }
+}
